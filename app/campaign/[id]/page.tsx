8667baab854d720741b38463e3a6f051acf0883d
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-04-04 22:56:30 -0300

    campigns id page fix

--- app/campaign/[id]/page.tsx
+++ app/campaign/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
-import DonationBox from '@/components/DonationBox'
+import DonationBox from '@/src/components/DonationBox'
 
 export default function CampaignDetail() {
 
