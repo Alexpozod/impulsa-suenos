b3d0cb10c001d947975a21921d52968db65a9e29
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-04-09 12:36:51 -0400

    mejora tip y pagefix

--- app/campaign/[id]/page.tsx
+++ app/campaign/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
-import DonationBox from '@/app/components/donationbox'
+import DonationBox from '@/app/components/DonationBox'
 
 export default function CampaignDetail() {
 
