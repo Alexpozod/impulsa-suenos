eb5018cdf933abe68433504947154c95a641abb4
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-04-22 11:46:49 -0400

    fix reconcile

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import type { ReactNode } from "react"
 
 import { Geist, Geist_Mono } from "next/font/google"
-import "@/app/globals.css"
+import "./globals.css"
 
 import Navbar from "@/app/components/Navbar"
 import Footer from "@/app/components/Footer"
