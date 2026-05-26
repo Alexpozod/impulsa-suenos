a05ba2fecb40853398444ce27b1e553a53a3b6de
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-05-26 11:42:10 -0400

    new page success

--- app/api/raffles/payment-return/route.ts
+++ app/api/raffles/payment-return/route.ts
@@ -3,13 +3,15 @@ import { NextResponse } from "next/server"
 export async function POST() {
 
   return NextResponse.redirect(
-    "https://www.impulsasuenos.com/raffles/payment/success"
+    "https://www.impulsasuenos.com/raffles/payment/success",
+    303
   )
 }
 
 export async function GET() {
 
   return NextResponse.redirect(
-    "https://www.impulsasuenos.com/raffles/payment/success"
+    "https://www.impulsasuenos.com/raffles/payment/success",
+    303
   )
 }
