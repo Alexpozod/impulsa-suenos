3181f658ac38dca9696b8a41b25753b5927112b5
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-05-27 14:09:41 -0400

    fix security

--- app/api/raffles/validate-ticket/route.ts
+++ app/api/raffles/validate-ticket/route.ts
@@ -16,11 +16,78 @@ const supabase =
       .SUPABASE_SERVICE_ROLE_KEY!
   )
 
+  const ipRequests = new Map<
+  string,
+  {
+    count: number
+    timestamp: number
+  }
+>()
+
 export async function GET(
   req: Request
 ) {
 
   try {
+
+    /* =========================
+   BASIC RATE LIMIT
+========================= */
+
+const forwardedFor =
+  req.headers.get(
+    "x-forwarded-for"
+  )
+
+const ip =
+  forwardedFor
+    ?.split(",")[0]
+    ?.trim() || "unknown"
+
+const now = Date.now()
+
+const current =
+  ipRequests.get(ip)
+
+if (
+  current &&
+  now - current.timestamp <
+    60_000
+) {
+
+  if (current.count >= 30) {
+
+    return NextResponse.json(
+
+      {
+        valid: false,
+        error:
+          "rate_limit"
+      },
+
+      {
+        status: 429
+      }
+    )
+  }
+
+  current.count += 1
+
+  ipRequests.set(
+    ip,
+    current
+  )
+
+} else {
+
+  ipRequests.set(ip, {
+
+    count: 1,
+
+    timestamp: now
+  })
+
+}
 
     const { searchParams } =
       new URL(req.url)
