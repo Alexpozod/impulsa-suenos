00c7717ed88140b677b24b45766116dc2aed5793
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-06-13 17:54:35 -0400

    guard migration

--- lib/raffles/payment/steps/lockPayment.ts
+++ lib/raffles/payment/steps/lockPayment.ts
@@ -3,5 +3,42 @@ import { PaymentProcessingContext } from "../types"
 export async function lockPayment(
   context: PaymentProcessingContext
 ) {
+
+  if (!context.payment) {
+
+    throw new Error(
+      "payment_not_found"
+    )
+
+  }
+
+  const status =
+    context.payment.status
+
+  if (
+
+    status === "approved"
+
+  ) {
+
+    throw new Error(
+      "payment_already_processed"
+    )
+
+  }
+
+  if (
+
+    status === "processing"
+
+  ) {
+
+    throw new Error(
+      "payment_already_processing"
+    )
+
+  }
+
   return context
+
 }
