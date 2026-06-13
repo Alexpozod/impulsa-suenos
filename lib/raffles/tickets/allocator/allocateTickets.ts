915ee97769e81158fcae8aafb63396af063e057a
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-06-13 14:55:18 -0400

    new page COUPON

--- lib/raffles/tickets/allocator/allocateTickets.ts
+++ lib/raffles/tickets/allocator/allocateTickets.ts
@@ -3,12 +3,60 @@ import {
   AllocateTicketsResult
 } from "./types"
 
+import { selectAvailableTickets }
+from "./selectAvailableTickets"
+
+import { reserveSelectedTickets }
+from "./reserveSelectedTickets"
+
 export async function allocateTickets(
+
   input: AllocateTicketsInput
+
 ): Promise<AllocateTicketsResult> {
 
-  throw new Error(
-    "allocateTickets_not_implemented"
-  )
+  const availableTickets =
+
+    await selectAvailableTickets(
+
+      input.raffleId,
+
+      input.quantity
+
+    )
+
+  if (
+
+    availableTickets.length <
+
+    input.quantity
+
+  ) {
+
+    throw new Error(
+
+      "not_enough_tickets_available"
+
+    )
+
+  }
+
+  const reservation =
+
+    await reserveSelectedTickets(
+
+      availableTickets.map(
+
+        t => t.id
+
+      ),
+
+      input.orderId,
+
+      input.buyerEmail
+
+    )
+
+  return reservation
 
 }
