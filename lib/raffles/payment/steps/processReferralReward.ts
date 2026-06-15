7079bc58c96242cadae9c7896929b3e036ec8482
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-06-15 18:28:05 -0400

    page payotu afilia btton

---
+++ lib/raffles/payment/steps/processReferralReward.ts
@@ -0,0 +1,13 @@
+import { PaymentProcessingContext } from "../types"
+
+export async function processReferralReward(
+  context: PaymentProcessingContext
+) {
+
+  console.log(
+    "STEP -> processReferralReward"
+  )
+
+  return context
+
+}
