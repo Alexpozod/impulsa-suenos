dc8d4dbbc34a544d8bdde3f7f3e3529e528d8181
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-06-24 18:09:32 -0400

    page promotion

--- app/middleware.ts
+++ app/middleware.ts
@@ -3,6 +3,8 @@ import type { NextRequest } from "next/server"
 
 export async function middleware(req: NextRequest) {
 
+  throw new Error("MIDDLEWARE_TEST")
+
   const host =
   req.headers.get("host") || ""
 
