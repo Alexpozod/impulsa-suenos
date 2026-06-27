092250f4477786ab6a0bdb311330dd120dd12085
Alexander Pozo <alexpozo@MacBook-Pro-de-Alexander.local>
2026-06-26 20:40:00 -0400

    new motor comercial

--- lib/raffles/payment/steps/processCommercialReward.ts
+++ lib/raffles/payment/steps/processCommercialReward.ts
@@ -1,69 +1,47 @@
 import { PaymentProcessingContext } from "../types"
 
 import {
-  processAffiliateCommission
+  processAffiliateCommission as processAffiliate
 } from "@/lib/raffles/affiliate/processAffiliateCommission"
 
 import {
-  processReferralReward
+  processReferralReward as processReferral
 } from "@/lib/raffles/referral/processReferralReward"
 
 export async function processCommercialReward(
-
   context: PaymentProcessingContext
-
 ) {
 
   if (
-
     !context.order ||
-
     !context.payment
-
   ) {
-
     return context
-
   }
 
   const tracking =
+    context.order?.metadata?.tracking ?? {}
 
-    context.order.metadata?.tracking ??
-
-    {}
-
-  switch (
-
-    tracking.commercialType
-
-  ) {
+  switch (tracking.commercialType) {
 
     case "affiliate":
 
-      await processAffiliateCommission({
+      await processAffiliate({
 
         payment_id:
-
           context.payment.id,
 
         raffle_id:
-
           context.order.raffle_id,
 
         order_id:
-
           context.order.id,
 
         amount:
-
           Number(
-
             context.payment.amount_clp ??
-
             context.payment.amount ??
-
             0
-
           )
 
       })
@@ -72,10 +50,8 @@ export async function processCommercialReward(
 
     case "referral":
 
-      await processReferralReward(
-
+      await processReferral(
         context
-
       )
 
       break
